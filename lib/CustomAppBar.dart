7303e375688e6d4cc1cb3e1f59aa05176a3eed25
lulu12329 <lukathrein94@gmail.com>
2021-07-23 22:00:25 +0200

    updated AppBar

--- lib/CustomAppBar.dart
+++ lib/CustomAppBar.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 class CustomAppBar {
   static AppBar build(GlobalKey<ScaffoldState> scaffoldKey, String title) {
     return AppBar(
-      leading: InkWell(
+      leading: Padding(
+        padding: EdgeInsets.only(left: 10),
+        child
+        : InkWell(
         child: Image(image: AssetImage("assets/images/SkotSys_Meteor.png")),
         onTap: () => scaffoldKey.currentState?.openDrawer(),
-      ),
+      ),),
       title: Text(title),
       backgroundColor: Colors.black,
     );
